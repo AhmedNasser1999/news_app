ac1cadbba8d85ff6dd0ce43c9d9425be12df74ab
Ahmed Nasser <ahmed.nasser1935@gmail.com>
2024-07-15 04:03:37 +0300

    Added Colors File

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bannerTitleTextColor = Colors.black;
+  static const Color categorieTitleTextColor = Colors.white;
+  static const Color textButtonColor = Color.fromARGB(255, 25, 134, 223);
+  static const Color scaffoldBackgroundColor = Colors.white;
+  static const Color trendingTimeAndSourceTextColor = Colors.grey;
+  static const Color trendingTitleTextColor = Colors.black;
+  static const Color trendingCategorieTextColor = Colors.grey;
+  static const Color brakingNewsTextColor = Colors.white;
+}
