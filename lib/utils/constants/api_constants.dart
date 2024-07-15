120c7e1804e7cb53919fa1fb8d6fea5c5c94b9cb
Ahmed Nasser <ahmed.nasser1935@gmail.com>
2024-07-15 03:36:14 +0300

    Added API constants file

---
+++ lib/utils/constants/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  static const String baseUrl = 'https://newsapi.org';
+
+  static const String trendingNewsEndpoint = '/v2/top-headlines';
+  static const String categoryNewsEndpoint = '/v2/top-headlines/sources';
+  static const String breakingNewsEndpoint = '/v2/top-headlines';
+}
